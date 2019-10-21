b4ff71710599423f2f9dfbb4368ab71cb859f9bd
Junaid Malik <jmalik981@gmail.com>
2019-10-21 13:42:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,4 @@
 def conference_badges
-  name = ["Arel"]
-  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   attendees.each do |badge|
     puts "Hello, my name is #{badge}"
   end
